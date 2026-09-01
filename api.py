e973fdf3b315ef99cd6d8ab9d8431932e1044294
ada-netizen <adaibrahim102@gmail.com>
2026-09-01 12:06:13 +0300

    Fix: Restore CORS allow_origins to fix frontend requests (Origin: null)

--- api.py
+++ api.py
@@ -51,12 +51,7 @@ app = FastAPI(title="Elektronik Okul API V2")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://localhost:8000",
-        "http://127.0.0.1",
-        "http://127.0.0.1:8000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
